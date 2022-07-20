3979921b89aecdb479fd22534046734e0f3f7d1d
Joe Chien <cchla1021@yahoo.com>
2022-07-20 02:32:52 +0000

    update YAML

--- reference-app/frontend/backup/app.py
+++ reference-app/frontend/backup/app.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template, request, jsonify
-
-import pymongo
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request
 
 import logging
 from jaeger_client import Config
 from prometheus_flask_exporter import PrometheusMetrics
-
-import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -21,22 +16,10 @@ by_full_path_counter = metrics.counter('full_path_counter', 'counting requests b
 by_endpoint_counter = metrics.counter('endpoint_counter', 'counting requestby endpoint', labels={
                                       'endpoint': lambda: request.endpoint})
 
-endpoints = ('', 'star', 'api')
-
-JAEGER_AGENT_HOST = os.getenv('JAEGER_AGENT_HOST', 'localhost')
-
-app.config["MONGO_DBNAME"] = "example-mongodb"
-app.config[
-    "MONGO_URI"
-] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
-
-mongo = PyMongo(app)
-
-logger = logging.getLogger(__name__)
-
 # Tracing Initialization
 
-def init_tracer(service_name="frontend-service"):
+
+def init_tracer(service_name="fronend-service"):
     logging.getLogger('').handlers = []
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
@@ -50,59 +33,17 @@ def init_tracer(service_name="frontend-service"):
 
     return config.initialize_tracer()
 
-tracer = init_tracer("frontend-service")
+
+tracer = init_tracer("fronend-service")
 
 
 @app.route("/")
 @by_full_path_counter
 @by_endpoint_counter
 def homepage():
-    app.logger.info('Hit the homepage')
     with tracer.start_span('homepage-span') as span:
-        span.set_tag('homepage-tag', '95')
-        return "Hello World"
-
-
-@app.route("/api")
-@by_full_path_counter
-@by_endpoint_counter
-def my_api():
-    app.logger.info('Hit the /api endpoint')
-    with tracer.start_span('my_api_span') as span:
-        span.set_tag('my_api-tag', '90')
-        answer = "something"
-        return jsonify(reponse=answer)
-
-
-@app.route('/star', methods=['POST'])
-@by_full_path_counter
-@by_endpoint_counter
-def add_star():
-    app.logger.info('Hit the /star endpoint')
-
-    with tracer.start_span('star_span') as span:
-        span.set_tag('star-tag', '80')
-
-        try:
-            star = mongo.db.stars
-            name = request.json['name']
-            distance = request.json['distance']
-            star_id = star.insert({'name': name, 'distance': distance})
-            new_star = star.find_one({'_id': star_id})
-            output = {'name': new_star['name'],
-                      'distance': new_star['distance']}
-            return jsonify({'result': output})
-        except Exception as e:
-            logger.error(f"Unable to add a star")
-            span.set_tag("http.status_code", "500")
-            print(e)
-
-
-@app.route('/error')
-@by_full_path_counter
-@by_endpoint_counter
-def oops():
-    return ':(', 500
+        span.set_tag('homepage-tag', '90')
+        return render_template("main.html")
 
 
 class InvalidUsage(Exception):
