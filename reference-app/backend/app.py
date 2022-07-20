6900ac37e4dbb05c35757664a079b8b1051b583e
Joe Chien <cchla1021@yahoo.com>
2022-07-20 01:48:41 +0000

    update backend-service

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,50 +1,123 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import os
+import pymongo
 import logging
+from flask_pymongo import PyMongo
+# Tracing
 from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+# from prometheus_flask_exporter import PrometheusMetrics
+# Since we're using gunicorn - https://github.com/rycus86/prometheus_flask_exporter/blob/master/examples/gunicorn-internal
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 from prometheus_flask_exporter import PrometheusMetrics
 
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+# Jaeger Tracing Config
+'''
+trace.set_tracer_provider(TracerProvider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "backend"})
+    )
+))
 
-# static information as metric. Adapted from https://github.com/rycus86/prometheus_flask_exporter/blob/master/examples/sample-signals/app/app.py
-metrics.info('app_info', 'Application info', version='1.0.3')
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(JaegerExporter())
+    )
 
-by_full_path_counter = metrics.counter('full_path_counter', 'counting requests by full path', labels={
-                                       'full_path': lambda: request.full_path})
+tracer = trace.get_tracer(__name__)
+'''
 
-by_endpoint_counter = metrics.counter('endpoint_counter', 'counting requestby endpoint', labels={
-                                      'endpoint': lambda: request.endpoint})
-
-# Tracing Initialization
-
-
-def init_tracer(service_name="backend-service"):
+# Configure Jaeger tracer
+def init_tracer(service):
     logging.getLogger('').handlers = []
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
     config = Config(
         config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
             'logging': True,
         },
-        service_name=service_name,
+        service_name=service,
         validate=True
     )
 
+    # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
+tracer = init_tracer('backend')
 
-tracer = init_tracer("backend-service")
+# Backend app
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+CORS(app)
 
+# FlaskInstrumentor().instrument_app(app, excluded_urls="metrics")
+# RequestsInstrumentor().instrument()
 
-@app.route("/")
-@by_full_path_counter
-@by_endpoint_counter
+metrics = GunicornInternalPrometheusMetrics(app, group_by='endpoint')
+# metrics = PrometheusMetrics(app, group_by='endpoint')
+
+# static information as metric
+metrics.info('backend', 'Backend App Metrics', version='1.0.3')
+
+# register extra metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths', labels={'path': lambda: request.path}
+    )
+)
+
+# custom metric to be applied to multiple endpoints
+endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+app.config['MONGO_DBNAME'] = 'example-mongodb'
+app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@endpoint_counter
 def homepage():
-    with tracer.start_span('homepage-span') as span:
-        span.set_tag('homepage-tag', '90')
-        return render_template("main.html")
+    with tracer.start_active_span('home-page'):
+        answer = "I'm on the home page"
+        return jsonify(response=answer)
 
+
+@app.route('/api')
+@endpoint_counter
+def my_api():
+    with tracer.start_span('my-api'):
+        answer = something # This will create an error
+        return jsonify(response=answer)
+
+@app.route('/star', methods=['POST'])
+@endpoint_counter
+def add_star():
+    with tracer.start_span('add star'):
+        star = mongo.db.stars
+        name = request.json['name']
+        distance = request.json['distance']
+        star_id = star.insert({'name': name, 'distance': distance})
+        new_star = star.find_one({'_id': star_id })
+        output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+        return jsonify({'result' : output})
 
 class InvalidUsage(Exception):
     status_code = 400
@@ -68,14 +141,12 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-
 @app.route("/403")
 def status_code_403():
     status_code = 403
     raise InvalidUsage(
         "Raising status code: {}".format(status_code), status_code=status_code
     )
-
 
 @app.route("/404")
 def status_code_404():
@@ -84,14 +155,12 @@ def status_code_404():
         "Raising status code: {}".format(status_code), status_code=status_code
     )
 
-
 @app.route("/500")
 def status_code_500():
     status_code = 500
     raise InvalidUsage(
         "Raising status code: {}".format(status_code), status_code=status_code
     )
-
 
 @app.route("/503")
 def status_code_503():
@@ -100,6 +169,5 @@ def status_code_503():
         "Raising status code: {}".format(status_code), status_code=status_code
     )
 
-
 if __name__ == "__main__":
-    app.run(threaded=True)
+    app.run()
